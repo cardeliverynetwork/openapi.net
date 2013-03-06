7df8df6f93b2a87fa5537b3857d631954d382e82
Chris Wallis <chris@inybble.com>
2013-03-06 09:20:57 -0800

    Added Export

---
+++ src/CarDeliveryNetwork.Api.Data/Export.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CarDeliveryNetwork.Api.Data
+{
+    /// <summary>
+    /// Describes an export file
+    /// </summary>
+    public class Export
+    {
+        /// <summary>
+        /// The Url of the export file
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
