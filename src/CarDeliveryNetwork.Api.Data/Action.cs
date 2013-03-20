37d8c488e888087f3eb5b6796eb322e5f73a4510
Chris Wallis <chris@inybble.com>
2013-03-20 06:07:08 -0700

    Implemented IApiEntity for Action

--- src/CarDeliveryNetwork.Api.Data/Action.cs
+++ src/CarDeliveryNetwork.Api.Data/Action.cs
@@ -4,7 +4,7 @@ namespace CarDeliveryNetwork.Api.Data
     /// <summary>
     /// Describes an action to be carried out on a resource.
     /// </summary>
-    public class Action
+    public class Action : IApiEntity
     {
         /// <summary>
         /// Name
@@ -14,6 +14,30 @@ namespace CarDeliveryNetwork.Api.Data
         /// <summary>
         /// Note
         /// </summary>
+        public string AssociatedId { get; set; }
+
+        /// <summary>
+        /// Note
+        /// </summary>
         public string Note { get; set; }
+
+        /// <summary>
+        /// Returns a serial representation of the object in JSON format.
+        /// </summary>
+        /// <returns>The serialized object.</returns>
+        public override string ToString()
+        {
+            return this.ToString(Types.MessageFormat.Json);
+        }
+
+        /// <summary>
+        /// Returns a serial representation of the object in the specified format.
+        /// </summary>
+        /// <param name="format">Format to serialize to.</param>
+        /// <returns>The serialized object.</returns>
+        public string ToString(Types.MessageFormat format)
+        {
+            return Serialization.Serialize(this, format);
+        }
     }
 }
