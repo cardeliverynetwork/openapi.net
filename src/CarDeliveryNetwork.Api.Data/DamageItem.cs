8c361803729079207e1de6b22ed13b17bd41b8d4
Chris Wallis <chris@inybble.com>
2013-03-27 15:57:48 -0700

    Added DamageItem and Vehicle.DamageAtPickup, Vehicle.DamageAtDropoff

---
+++ src/CarDeliveryNetwork.Api.Data/DamageItem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CarDeliveryNetwork.Api.Data
+{
+    /// <summary>
+    /// A Car Delivery Network Damage Item entity
+    /// </summary
+    public class DamageItem : ApiEntityBase<Bid>
+    {
+        public abstract class DamageProperty : IApiEntity
+        {
+            /// <summary>
+            /// Description
+            /// </summary>
+            public virtual string CommonId { get; set; }
+
+            /// <summary>
+            /// Description
+            /// </summary>
+            public virtual string Description { get; set; }
+
+            /// <summary>
+            /// Returns a serial representation of the object in JSON format.
+            /// </summary>
+            /// <returns>The serialized object.</returns>
+            public override string ToString()
+            {
+                return this.ToString(Types.MessageFormat.Json);
+            }
+
+            /// <summary>
+            /// Returns a serial representation of the object in the specified format.
+            /// </summary>
+            /// <param name="format">Format to serialize to.</param>
+            /// <returns>The serialized object.</returns>
+            public string ToString(Types.MessageFormat format)
+            {
+                return Serialization.Serialize(this, format);
+            }
+        }
+
+        /// <summary>
+        /// A Car Delivery Network DamageItem.Area entity
+        /// </summary>
+        public class DamageArea : DamageProperty { }
+
+        /// <summary>
+        /// A Car Delivery Network DamageItem.Type entity
+        /// </summary>
+        public class DamageType : DamageProperty { }
+
+        /// <summary>
+        /// A Car Delivery Network DamageItem.Severity entity
+        /// </summary>
+        public class DamageSeverity : DamageProperty { }
+
+        /// <summary>
+        /// Damage Area
+        /// </summary>
+        public virtual DamageItem.DamageArea Area { get; set; }
+
+        /// <summary>
+        /// Damage Type
+        /// </summary>
+        public virtual DamageItem.DamageType Type { get; set; }
+
+        /// <summary>
+        /// Damage Severity
+        /// </summary>
+        public virtual DamageItem.DamageSeverity Severity { get; set; }  
+    }
+
+    /// <summary>
+    /// A collection of Car Delivery Network DamageItem entities.
+    /// </summary>
+    [CollectionDataContract]
+    public class DamageItems : ApiEntityCollectionBase<DamageItem, DamageItems>, IApiEntity
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.DamageItems"/> class
+        /// that is empty and has the default initial capacity.
+        /// </summary>
+        public DamageItems() : base() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.DamageItems"/> class
+        /// that is empty and has the specified initial capacity.
+        /// </summary>
+        /// <param name="capacity">The number of elements that the new list can initially store</param>
+        public DamageItems(int capacity) : base(capacity) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.DamageItems"/> class
+        /// that contains elements copied from the specified collection and has sufficient
+        /// capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="DamageItems">The collection of DamageItems whose elements are copied to the new collection.</param>
+        public DamageItems(List<DamageItem> DamageItems) : base(DamageItems) { }
+    }
+}
--- src/CarDeliveryNetwork.Api.Data/Vehicle.cs
+++ src/CarDeliveryNetwork.Api.Data/Vehicle.cs
@@ -1,12 +1,13 @@
 ﻿
+using System.Collections.Generic;
 namespace CarDeliveryNetwork.Api.Data
 {
     /// <summary>
     /// A Car Delivery Network Vehicle entity. You must specify a VIN or Registration. 
     /// </summary>
     /// <remarks> 
-	/// If you do not want to specify an actual VIN you should specify a placeholder (1,2,3 etc) 
-	/// of less than 6 characters and the system will force the capture of a valid VIN
+    /// If you do not want to specify an actual VIN you should specify a placeholder (1,2,3 etc) 
+    /// of less than 6 characters and the system will force the capture of a valid VIN
     /// </remarks>	
     public class Vehicle : ApiEntityBase<Vehicle>
     {
@@ -41,13 +42,40 @@ namespace CarDeliveryNetwork.Api.Data
         public virtual string Location { get; set; }
 
         /// <summary>
-        /// Optional (50) - A movement number specific to this vehicle
+        /// Optional (50) - A movement number specific to this Vehicle
         /// </summary>
         public virtual string MovementNumber { get; set; }
 
         /// <summary>
-        /// Optional (255) - Notes relating to this vehicle.
+        /// Optional (255) - Notes relating to this Vehicle.
         /// </summary>
         public virtual string Notes { get; set; }
+
+        /// <summary>
+        /// Readonly - A collection of damage items recorded at pickup
+        /// </summary>
+        public virtual List<DamageItem> DamageAtPickup { get; set; }
+
+        /// <summary>
+        /// Readonly - A collection of EXTRA damage items recorded at dropoff
+        /// </summary>
+        public virtual List<DamageItem> DamageAtDropoff { get; set; }
+
+        /// <summary>
+        /// Vehicle
+        /// </summary>
+        public Vehicle()
+        {
+            InitObjects();
+        }
+
+        /// <summary>
+        /// Initializes the child objects associated with this Vehicle.
+        /// </summary>
+        protected virtual void InitObjects()
+        {
+            DamageAtPickup = new List<DamageItem>();
+            DamageAtDropoff = new List<DamageItem>();
+        }
     }
 }
