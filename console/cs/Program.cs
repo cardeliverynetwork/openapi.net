82826a0c82bceff9020f3b1eed996c70d18563d4
Chris Wallis <chris@inybble.com>
2013-03-28 06:51:11 -0700

    Updated cs console app for nomalised endpoints

--- console/cs/Program.cs
+++ console/cs/Program.cs
@@ -25,14 +25,13 @@ namespace console
             var api = new OpenApi(ServiceUrl, ServiceApiKey);
 
             // Create a job
-           // var newjob = api.CreateJob(GetTestJob());
+            var newjob = api.CreateJob(GetTestJob());
 
             // Get some details about a job
             var targetJobId = 9697;
             var job = api.GetJob(targetJobId);
             var docs = api.GetJobDocuments(targetJobId);
             api.CancelJob(targetJobId, "Cancelled by customer.");
-
         }
 
         private static Job GetTestJob()
@@ -44,10 +43,6 @@ namespace console
             newjob.CustomerReference = "REF123";
             newjob.Notes = "These are the notes";
             newjob.ServiceRequired = ServiceType.Transported;
-            newjob.RequestedPickup = DateTime.Today + TimeSpan.FromDays(1);
-            newjob.RequestedPickupIsExactDate = true;
-            newjob.RequestedDropoff = DateTime.Today + TimeSpan.FromDays(3);
-            newjob.RequestedDropoffIsExactDate = false;
 
             // Setup the customer record
             newjob.Customer.QuickCode = "COMPANY1";
@@ -62,26 +57,30 @@ namespace console
             newjob.Customer.Email = "jsmith@example.com";
 
             // Setup the pick-up details
-            newjob.Pickup.QuickCode = "WHVCS";
-            newjob.Pickup.Contact = "Paul Pie";
-            newjob.Pickup.OrganisationName = "We Have Cars Ltd.";
-            newjob.Pickup.AddressLines = "4 Well Dean\r\nCastlefields";
-            newjob.Pickup.City = "Prudhoe";
-            newjob.Pickup.StateRegion = "Northumberland";
-            newjob.Pickup.ZipPostCode = "NE42 5QQ";
-            newjob.Pickup.Phone = "456 456 456";
-            newjob.Pickup.Notes = "These are the pickup notes";
+            newjob.Pickup.RequestedDate = DateTime.Today + TimeSpan.FromDays(1);
+            newjob.Pickup.RequestedDateIsExact = true;
+            newjob.Pickup.Destination.QuickCode = "WHVCS";
+            newjob.Pickup.Destination.Contact = "Paul Pie";
+            newjob.Pickup.Destination.OrganisationName = "We Have Cars Ltd.";
+            newjob.Pickup.Destination.AddressLines = "4 Well Dean\r\nCastlefields";
+            newjob.Pickup.Destination.City = "Prudhoe";
+            newjob.Pickup.Destination.StateRegion = "Northumberland";
+            newjob.Pickup.Destination.ZipPostCode = "NE42 5QQ";
+            newjob.Pickup.Destination.Phone = "456 456 456";
+            newjob.Pickup.Destination.Notes = "These are the pickup notes";
 
             // Setup the drop-off details
-            newjob.Dropoff.QuickCode = "WNDCS";
-            newjob.Dropoff.Contact = "Peter Piper";
-            newjob.Dropoff.OrganisationName = "We Need Cars Ltd.";
-            newjob.Dropoff.AddressLines = "1 Yeomans Court";
-            newjob.Dropoff.City = "Hemel Hempstead";
-            newjob.Dropoff.StateRegion = "Hertfordshire";
-            newjob.Dropoff.ZipPostCode = "HP2 7GJ";
-            newjob.Dropoff.Phone = "789 789 789";
-            newjob.Dropoff.Notes = "These are the drop-off notes";
+            newjob.Dropoff.RequestedDate = DateTime.Today + TimeSpan.FromDays(3);
+            newjob.Dropoff.RequestedDateIsExact = false;
+            newjob.Dropoff.Destination.QuickCode = "WNDCS";
+            newjob.Dropoff.Destination.Contact = "Peter Piper";
+            newjob.Dropoff.Destination.OrganisationName = "We Need Cars Ltd.";
+            newjob.Dropoff.Destination.AddressLines = "1 Yeomans Court";
+            newjob.Dropoff.Destination.City = "Hemel Hempstead";
+            newjob.Dropoff.Destination.StateRegion = "Hertfordshire";
+            newjob.Dropoff.Destination.ZipPostCode = "HP2 7GJ";
+            newjob.Dropoff.Destination.Phone = "789 789 789";
+            newjob.Dropoff.Destination.Notes = "These are the drop-off notes";
 
             // Add some vehicles
             newjob.Vehicles.Add(new Vehicle()
