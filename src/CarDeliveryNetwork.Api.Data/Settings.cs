57ff48d16a4083189bd841ed32e6d34e2be8495c
Chris Wallis <chris@inybble.com>
2013-04-16 07:07:08 -0700

    Added Settings for CDN instance settings

---
+++ src/CarDeliveryNetwork.Api.Data/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarDeliveryNetwork.Api.Data
+{
+    public class Settings
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// NomadUrl
+        /// </summary>
+        public string NomadUrl { get; set; }
+
+        /// <summary>
+        /// NomadJsonUrl
+        /// </summary>
+        public string NomadJsonUrl { get; set; }
+    }
+}
