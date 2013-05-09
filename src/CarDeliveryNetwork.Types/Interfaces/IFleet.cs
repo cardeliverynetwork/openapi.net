4054aa283529b1c804b991c92d686245b2a3feb2
Chris Wallis <chris@inybble.com>
2013-05-09 09:06:19 -0700

    Added IFleet Cmac, CanTransport and CanDrive properties

--- src/CarDeliveryNetwork.Types/Interfaces/IFleet.cs
+++ src/CarDeliveryNetwork.Types/Interfaces/IFleet.cs
@@ -23,6 +23,21 @@ namespace CarDeliveryNetwork.Types.Interfaces
         string DisplayName { get; }
 
         /// <summary>
+        /// CMAC
+        /// </summary>
+        string Cmac { get; }
+
+        /// <summary>
+        /// Fleet can do transported work
+        /// </summary>
+        bool CanTransport { get; }
+
+        /// <summary>
+        /// Fleet can do driven work
+        /// </summary>
+        bool CanDrive { get; }
+
+        /// <summary>
         /// MenuProfile
         /// </summary>
         FleetMenuProfile MenuProfile { get; set; }
