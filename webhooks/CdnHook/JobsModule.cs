71a4e0e9ce9814afe628a9a0a287cc9a675756f2
Chris Wallis <chris@inybble.com>
2013-05-09 01:53:28 -0700

    Added webhook stub that just returns 200 OK

---
+++ webhooks/CdnHook/JobsModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace CdnHook
+{
+    public class JobsModule : NancyModule
+    {
+        public JobsModule() : base("/jobs")
+        {
+            Post["/{id}"] = parameters =>
+            {
+                var id = parameters.id;
+                return HttpStatusCode.OK;
+            };
+        }
+    }
+}
---
+++ webhooks/CdnHook/RootModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace CdnHook
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule() 
+        {
+            Get["/"] = parameters => "Car Delivery Network ASP.NET + Nancy WebHook Stub";
+        }
+    }
+}
