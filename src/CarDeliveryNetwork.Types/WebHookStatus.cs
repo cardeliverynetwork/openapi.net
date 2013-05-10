f13037dc2fd593af4eee8c599888705cc25b59db
Chris Wallis <chris@inybble.com>
2013-05-10 08:16:48 -0700

    Added WebHookStatus

---
+++ src/CarDeliveryNetwork.Types/WebHookStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum of Car Delivery Network WebHook status'.
+    /// </summary>
+    public enum WebHookStatus
+    {
+        /// <summary>
+        /// Hook is sent and awaiting a response
+        /// </summary>
+        Sent,
+
+        /// <summary>
+        /// Hook is retrying
+        /// </summary>
+        Retrying = 10,
+
+        /// <summary>
+        /// Hook succeeded
+        /// </summary>
+        Succeeded = 20,
+
+        /// <summary>
+        /// Hook failed
+        /// </summary>
+        Failed = 30,
+    }
+}
