193f4136f2ec46394de3f4a56b9736a6dc25e21c
Chris Wallis <chris@inybble.com>
2013-05-10 08:17:00 -0700

    Added WebHook

---
+++ src/CarDeliveryNetwork.Api.Data/WebHook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CarDeliveryNetwork.Types;
+
+namespace CarDeliveryNetwork.Api.Data
+{
+    /// <summary>
+    /// A Car Delivery Network WebHook entity.
+    /// </summary>
+    public class WebHook : ApiEntityBase<WebHook>
+    {
+        /// <summary>
+        /// Date the hook was first sent
+        /// </summary>
+        public virtual DateTime? FirstSent { get; set; }
+
+        /// <summary>
+        /// Date the current status was applied
+        /// </summary>
+        public virtual DateTime? StatusTime { get; set; }
+
+        /// <summary>
+        /// The current status
+        /// </summary>
+        public virtual WebHookStatus Status { get; set; }
+
+        /// <summary>
+        /// HttpMethod 
+        /// </summary>
+        public virtual string HttpMethod { get; set; }
+
+        /// <summary>
+        /// Url
+        /// </summary>
+        public virtual string Url { get; set; }
+
+        /// <summary>
+        /// DataFormat 'json' or 'xml'
+        /// </summary>
+        public virtual string DataFormat { get; set; }
+
+        /// <summary>
+        /// RequestBody
+        /// </summary>
+        public virtual string RequestBody { get; set; }
+
+        /// <summary>
+        /// LastError
+        /// </summary>
+        public virtual string LastError { get; set; }
+    }
+}
