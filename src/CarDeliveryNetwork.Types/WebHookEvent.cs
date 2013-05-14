c6b32fbcf951d68466d77bb34434ada20a981f74
Chris Wallis <chris@inybble.com>
2013-05-14 01:25:49 -0700

    Added WebHookEvent

---
+++ src/CarDeliveryNetwork.Types/WebHookEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum of Car Delivery Network WebHook status'.
+    /// </summary>
+    public enum WebHookEvent
+    {
+        /// <summary>
+        /// WebHook Event for a stop where vehicles were picked up
+        /// </summary>
+        PickupStop,
+
+        /// <summary>
+        /// WebHook Event for a stop where vehicles were dropped off
+        /// </summary>
+        DropoffStop
+    }
+}
