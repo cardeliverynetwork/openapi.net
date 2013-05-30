cf9697eba25cf58bddcc3d1dd515c75e35653703
Chris Wallis <chris@inybble.com>
2013-05-30 06:14:33 -0700

    Added enum AlertType

---
+++ src/CarDeliveryNetwork.Types/AlertType.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum of alert types
+    /// </summary>
+    public enum AlertType
+    {
+        Adhoc,
+        VehicleAdded,
+        VehicleNotCollected,
+        AdditionalDamage,
+        BrokerChangeJobDetail,
+        Question,
+        Answer,
+        JobChanged,
+        BrokerAddVehicle,
+        JobOpportunity,
+        InstantQuote,
+        AllocatedJob,
+        WinningBid,
+        CustomerAddedJob,
+        CustomerJobBooked,
+        DatesOverridden,
+        OutOfCredit
+    }
+}
