b3f5d1d7e8756dcbec0b5142a00d2d50257ce0cf
Chris Wallis <chris@inybble.com>
2013-05-31 09:28:29 -0700

    Added NamespaceDoc for sandcastle documentation

---
+++ src/CarDeliveryNetwork.Types/Interfaces/NamespaceDoc.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CarDeliveryNetwork.Types.Interfaces
+{
+    /// <summary>
+    /// The <see cref="CarDeliveryNetwork.Types.Interfaces"/> namespace contains interfaces visible to Car Delivery Network and its APIs.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
