fe3cf80984c5edab9870c0bc1cbedd64a6b8236c
Chris Wallis <chris@inybble.com>
2013-07-18 08:04:12 -0700

    Added NUnit test project

---
+++ cdnlink/CdnLink.Tests/TestHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using NUnit.Framework;
+
+namespace CdnLink.Tests
+{
+    [TestFixture]
+    public class TestHelpers
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetSetting_Null()
+        {
+            Helpers.GetSetting(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(SettingsPropertyNotFoundException))]
+        public void GetSetting_Empty()
+        {
+            Helpers.GetSetting("");
+        }
+    }
+}
