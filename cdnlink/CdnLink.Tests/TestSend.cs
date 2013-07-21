d96388d69cf5557b1c24fa5887c280e5c78f75b8
Chris Wallis <chris@inybble.com>
2013-07-21 06:43:34 -0700

    Added Send test fixture

---
+++ cdnlink/CdnLink.Tests/TestSend.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace CdnLink.Tests
+{
+    [TestFixture]
+    public class TestSend
+    {
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            var db = new CdnLinkDataContext(Settings.GetConnetionString());
+            db.CdnSendVehicles.DeleteAllOnSubmit(db.CdnSendVehicles);
+            db.SubmitChanges();
+
+            db.CdnSendLoads.DeleteAllOnSubmit(db.CdnSendLoads);
+            db.SubmitChanges();
+
+            db.CdnSends.DeleteAllOnSubmit(db.CdnSends);
+            db.SubmitChanges();
+        }
+
+        [Test]
+        public void Send()
+        {
+ 
+        }
+    }
+}
