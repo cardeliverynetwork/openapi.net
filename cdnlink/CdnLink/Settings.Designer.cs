de5be1e81ce7fe51fc7968b3a8bf3e8322d6b838
Chris Wallis <chris@inybble.com>
2013-07-29 08:22:04 -0700

    Removed generated connection string

--- cdnlink/CdnLink/Settings.Designer.cs
+++ cdnlink/CdnLink/Settings.Designer.cs
@@ -88,15 +88,5 @@ namespace CdnLink {
                 return ((string)(this["CDNLINK_API_KEY"]));
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=INY-BUNSTER;Initial Catalog=CdnLink;Integrated Security=True")]
-        public string CdnLinkConnectionString {
-            get {
-                return ((string)(this["CdnLinkConnectionString"]));
-            }
-        }
     }
 }
