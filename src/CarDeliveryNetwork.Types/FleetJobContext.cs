cbed29bf545c00324888449686e56eea1fa52af2
Chris Wallis <chris@inybble.com>
2013-09-12 07:34:02 -0700

    Added FleetJobContext for searches

---
+++ src/CarDeliveryNetwork.Types/FleetJobContext.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// An enumeration that specifies the authority of an fleet entity
+    /// </summary>
+    public enum FleetJobContext
+    {
+        /// <summary>
+        /// The original owner of the job
+        /// </summary>
+        Originator = 0,
+
+        /// <summary>
+        /// The entity that carries out the job
+        /// </summary>
+        Carrier = 1,
+    }
+}
