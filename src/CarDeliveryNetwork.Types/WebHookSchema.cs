ed6caeffcb4231f34e5ea4516ef62a5bd61e29e5
Chris Wallis <chris@inybble.com>
2013-10-25 06:47:47 -0700

    Docs

--- src/CarDeliveryNetwork.Types/WebHookSchema.cs
+++ src/CarDeliveryNetwork.Types/WebHookSchema.cs
@@ -1,9 +1,19 @@
 ﻿
 namespace CarDeliveryNetwork.Types
 {
+    /// <summary>
+    /// CDN Data schemes
+    /// </summary>
     public enum WebHookSchema
     {
+        /// <summary>
+        /// The CDN Schema
+        /// </summary>
         Cdn = 0,
+
+        /// <summary>
+        /// The Fenkell05 Schema
+        /// </summary>
         Fenkell05 = 10
     }
 }
