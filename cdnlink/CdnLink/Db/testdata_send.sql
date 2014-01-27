3d1a79859308d424f3923e45c81e20ad1d225e4f
Chris Wallis <chris@inybble.com>
2014-01-27 05:09:45 -0800

    Layout (Tabs to spaces)

--- cdnlink/CdnLink/Db/testdata_send.sql
+++ cdnlink/CdnLink/Db/testdata_send.sql
@@ -2,34 +2,34 @@
 -- Insert test sends
 INSERT INTO CdnSendLoads 
 (
-	LoadId,
-	ServiceRequired,
-	CustomerQuickCode,
-	CustomerContact,
-	CustomerOrganisationName,
-	CustomerAddressLines,
-	CustomerCity,
-	CustomerStateRegion,
-	CustomerZipPostCode,
-	PickupQuickCode,
-	PickupContact,
-	PickupOrganisationName,
-	PickupAddressLines,
-	PickupCity,
-	PickupStateRegion,
-	PickupZipPostCode,
-	PickupRequestedDate,
-	PickupRequestedDateIsExact,
-	DropoffQuickCode,
-	DropoffContact,
-	DropoffOrganisationName,
-	DropoffAddressLines,
-	DropoffCity,
-	DropoffStateRegion,
-	DropoffZipPostCode,
-	DropoffRequestedDate,
-	DropoffRequestedDateIsExact,
-	AllocatedCarrierScac
+    LoadId,
+    ServiceRequired,
+    CustomerQuickCode,
+    CustomerContact,
+    CustomerOrganisationName,
+    CustomerAddressLines,
+    CustomerCity,
+    CustomerStateRegion,
+    CustomerZipPostCode,
+    PickupQuickCode,
+    PickupContact,
+    PickupOrganisationName,
+    PickupAddressLines,
+    PickupCity,
+    PickupStateRegion,
+    PickupZipPostCode,
+    PickupRequestedDate,
+    PickupRequestedDateIsExact,
+    DropoffQuickCode,
+    DropoffContact,
+    DropoffOrganisationName,
+    DropoffAddressLines,
+    DropoffCity,
+    DropoffStateRegion,
+    DropoffZipPostCode,
+    DropoffRequestedDate,
+    DropoffRequestedDateIsExact,
+    AllocatedCarrierScac
 
 --  Optional allocation fields.  Uncomment as required
 --	
@@ -38,17 +38,17 @@ INSERT INTO CdnSendLoads
 ) 
 VALUES 
 (
-	'load1',                                  -- The use-once, unique to shipper load Id
-	1,                                        -- Service required Transported (0 for driven)
-	'CDN',                                    -- The customer quick code
-	'Wayne Pollock',                             
-	'Car Delivery Network, Inc.',
-	'7280 NW 87th Terr.',
-	'Kansas City',
-	'MO',
-	'64153',
-	'MS123',                                  -- The pickup quick code
-	'Bill',
+    'load1',                                  -- The use-once, unique to shipper load Id
+    1,                                        -- Service required Transported (0 for driven)
+    'CDN',                                    -- The customer quick code
+    'Wayne Pollock',                             
+    'Car Delivery Network, Inc.',
+    '7280 NW 87th Terr.',
+    'Kansas City',
+    'MO',
+    '64153',
+    'MS123',                                  -- The pickup quick code
+    'Bill',
     'Mirosoft',
     '1065 La Avenida',
     'Mountain View',
@@ -56,15 +56,15 @@ VALUES
     '94043',
     convert(varchar(10), getdate(), 120),     -- Date only, Today	
     1,                                        -- Exact date
-	'AP4242',                                 -- The dropoff quick code
+    'AP4242',                                 -- The dropoff quick code
     'Steve',
     'Apple',
     '1 Infinite Loop',
     'Cupertino',
     'CA',
-	'95014',
-	convert(varchar(10), getdate() + 2, 120), -- Date only, Today + 2 days
-	0,                                        -- Non exact date (deliver by this date)
+    '95014',
+    convert(varchar(10), getdate() + 2, 120), -- Date only, Today + 2 days
+    0,                                        -- Non exact date (deliver by this date)
     'SAC'                                     -- Carrier to allocate to
 
 --  Optional allocation fields.  Uncomment as required
@@ -84,48 +84,48 @@ VALUES ('load1', GETDATE(), 10);
 
 INSERT INTO CdnSendLoads 
 (
-	LoadId,
-	ServiceRequired,
-	CustomerQuickCode,
-	CustomerContact,
-	CustomerOrganisationName,
-	CustomerAddressLines,
-	CustomerCity,
-	CustomerStateRegion,
-	CustomerZipPostCode,
-	PickupQuickCode,
-	PickupContact,
-	PickupOrganisationName,
-	PickupAddressLines,
-	PickupCity,
-	PickupStateRegion,
-	PickupZipPostCode,
-	PickupRequestedDate,
-	PickupRequestedDateIsExact,
-	DropoffQuickCode,
-	DropoffContact,
-	DropoffOrganisationName,
-	DropoffAddressLines,
-	DropoffCity,
-	DropoffStateRegion,
-	DropoffZipPostCode,
-	DropoffRequestedDate,
-	DropoffRequestedDateIsExact,
-	AllocatedCarrierScac
+    LoadId,
+    ServiceRequired,
+    CustomerQuickCode,
+    CustomerContact,
+    CustomerOrganisationName,
+    CustomerAddressLines,
+    CustomerCity,
+    CustomerStateRegion,
+    CustomerZipPostCode,
+    PickupQuickCode,
+    PickupContact,
+    PickupOrganisationName,
+    PickupAddressLines,
+    PickupCity,
+    PickupStateRegion,
+    PickupZipPostCode,
+    PickupRequestedDate,
+    PickupRequestedDateIsExact,
+    DropoffQuickCode,
+    DropoffContact,
+    DropoffOrganisationName,
+    DropoffAddressLines,
+    DropoffCity,
+    DropoffStateRegion,
+    DropoffZipPostCode,
+    DropoffRequestedDate,
+    DropoffRequestedDateIsExact,
+    AllocatedCarrierScac
 ) 
 VALUES 
 (
-	'load2',
-	1,
-	'CDN',
-	'Wayne Pollock',
-	'Car Delivery Network, Inc.',
-	'7280 NW 87th Terr.',
-	'Kansas City',
-	'MO',
-	'64153',
-	'MS123',
-	'Bill',
+    'load2',
+    1,
+    'CDN',
+    'Wayne Pollock',
+    'Car Delivery Network, Inc.',
+    '7280 NW 87th Terr.',
+    'Kansas City',
+    'MO',
+    '64153',
+    'MS123',
+    'Bill',
     'Mirosoft',
     '1065 La Avenida',
     'Mountain View',
@@ -133,16 +133,16 @@ VALUES
     '94043',
     convert(varchar(10), getdate(), 120),	
     0,
-	'AP4242',
+    'AP4242',
     'Steve',
     'Apple',
     '1 Infinite Loop',
     'Cupertino',
     'CA',
-	'95014',
+    '95014',
     convert(varchar(10), getdate() + 2, 120),
-	0,
-	'SAC'
+    0,
+    'SAC'
 );
 
 INSERT INTO CdnSendVehicles(LoadId, Make, Model, Vin)
