6f0f55220373d1c21f6f7c2e495cc4e6e839eb87
Chris Wallis <chris@inybble.com>
2014-02-05 05:10:50 -0800

    Added fields to export rather than passing on URL

--- src/CarDeliveryNetwork.Api.Data/Export.cs
+++ src/CarDeliveryNetwork.Api.Data/Export.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace CarDeliveryNetwork.Api.Data
 {
     /// <summary>
@@ -6,6 +7,31 @@ namespace CarDeliveryNetwork.Api.Data
     /// </summary>
     public class Export
     {
+        /// <summary>
+        /// The type of things we're exporting
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// File format for the export
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// A list of Ids of the things we're exporting
+        /// </summary>
+        public string Ids { get; set; }
+
+        /// <summary>
+        /// Whether to indicate that the exported thing has been exported
+        /// </summary>
+        public bool MarkAsExported { get; set; }
+
+        /// <summary>
+        /// UTC Time of export
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+
         /// <summary>
         /// The Url of the export file
         /// </summary>
