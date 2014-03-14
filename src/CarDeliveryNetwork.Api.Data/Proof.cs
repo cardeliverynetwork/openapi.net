fdf1bad4c9ce1a3e4951e86d17d3ca6055ed0d6f
Chris Wallis <chris@inybble.com>
2014-03-14 16:49:29 +0100

    Added URL and deserialisation to Proof

--- src/CarDeliveryNetwork.Api.Data/Proof.cs
+++ src/CarDeliveryNetwork.Api.Data/Proof.cs
@@ -1,4 +1,5 @@
 ﻿
+using CarDeliveryNetwork.Types;
 namespace CarDeliveryNetwork.Api.Data
 {
     public class Proof : IApiEntity
@@ -52,6 +53,11 @@ namespace CarDeliveryNetwork.Api.Data
         public virtual string Driver { get; set; }
 
         /// <summary>
+        /// The Url to this Proof
+        /// </summary>
+        public virtual string Url { get; set;}
+
+        /// <summary>
         /// Returns a serial representation of the object in JSON format.
         /// </summary>
         /// <returns>The serialized object.</returns>
@@ -68,6 +74,27 @@ namespace CarDeliveryNetwork.Api.Data
         public string ToString(Types.MessageFormat format)
         {
             return Serialization.Serialize(this, format);
+        }
+
+        /// <summary>
+        /// Reads the JSON document and returns the deserialized object.
+        /// </summary>
+        /// <param name="serializedObject">The JSON serialized object.</param>
+        /// <returns>The deserialized object.</returns>
+        public static Proof FromString(string serializedObject)
+        {
+            return Serialization.Deserialise<Proof>(serializedObject, MessageFormat.Json);
+        }
+
+        /// <summary>
+        /// Reads the JSON or XML document and returns the deserialized object.
+        /// </summary>
+        /// <param name="serializedObject">The serialized object.</param>
+        /// <param name="format">The format of the serialized object.</param>
+        /// <returns>The deserialized object.</returns>
+        public static Proof FromString(string serializedObject, MessageFormat format)
+        {
+            return Serialization.Deserialise<Proof>(serializedObject, format);
         }
     }
 }
