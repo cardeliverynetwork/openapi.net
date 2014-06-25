6f5080fdd7b35c61b9a7e1ac5a5d54e7a6b93080
Chris Wallis <chris@inybble.com>
2014-06-25 12:26:28 +0200

    Added 8bit (uses varchar) upgrade script

---
+++ cdnlink/CdnLink/Db/upgrade_8bit.sql
@@ -0,0 +1,76 @@
+﻿SET ANSI_NULLS ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'PickupProofDocUrl' and Object_ID = Object_ID(N'CdnReceivedLoads'))
+BEGIN
+   ALTER TABLE CdnReceivedLoads
+ADD 
+	[PickupProofDocUrl] [varchar](1000) NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'DropoffProofDocUrl' and Object_ID = Object_ID(N'CdnReceivedLoads'))
+BEGIN
+   ALTER TABLE CdnReceivedLoads
+ADD 
+	[DropoffProofDocUrl] [varchar](1000) NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'Action' and Object_ID = Object_ID(N'CdnSends'))
+BEGIN
+   ALTER TABLE CdnSends
+ADD 
+	[Action] [int] NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'Type' and Object_ID = Object_ID(N'CdnReceivedDocuments'))
+BEGIN
+   ALTER TABLE CdnReceivedDocuments
+ADD 
+	[Type] [int] NULL,
+	[FriendlyType] [varchar] (25) NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'ContractedCarrierScac' and Object_ID = Object_ID(N'CdnSendLoads'))
+BEGIN
+   ALTER TABLE CdnSendLoads
+ADD 
+	[ContractedCarrierScac] [varchar](10) NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'ContractedCarrierScac' and Object_ID = Object_ID(N'CdnReceivedLoads'))
+BEGIN
+   ALTER TABLE CdnReceivedLoads
+ADD 
+	[ContractedCarrierScac] [varchar](10) NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'AssignedTruckRemoteId' and Object_ID = Object_ID(N'CdnSendLoads'))
+BEGIN
+   ALTER TABLE CdnSendLoads
+ADD 
+	[AssignedTruckRemoteId] [varchar](40) NULL
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+            WHERE Name = N'AssignedTruckRemoteId' and Object_ID = Object_ID(N'CdnReceivedLoads'))
+BEGIN
+   ALTER TABLE CdnReceivedLoads
+ADD 
+	[AssignedTruckRemoteId] [varchar](40) NULL
+END
+GO
+
