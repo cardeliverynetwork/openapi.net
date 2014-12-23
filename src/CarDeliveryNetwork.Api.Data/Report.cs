199c56f57c1b68803a12ae156e52ca43e35fc74a
Chris Wallis <chris@inybble.com>
2014-12-23 12:08:04 +0100

    Added Report class

---
+++ src/CarDeliveryNetwork.Api.Data/Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarDeliveryNetwork.Api.Data
+{
+    public class Report
+    {
+        public List<List<object>> Table { get; set; }
+    }
+}
