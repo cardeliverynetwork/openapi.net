777085d350060b9b6ff495b6a86b6c52ea256649
Chris Wallis <chris@inybble.com>
2015-03-30 12:49:42 +0200

    Serialisation class now public and commented

--- src/CarDeliveryNetwork.Api.Data/Serialization.cs
+++ src/CarDeliveryNetwork.Api.Data/Serialization.cs
@@ -6,8 +6,17 @@ using CarDeliveryNetwork.Types;
 
 namespace CarDeliveryNetwork.Api.Data
 {
-    static class Serialization
+    /// <summary>
+    /// Helper class for object serialisation and deserialisation
+    /// </summary>
+    public static class Serialization
     {
+        /// <summary>
+        /// Serialised the specified object in the specified format
+        /// </summary>
+        /// <param name="o">The object to serialise</param>
+        /// <param name="format">The format to serialise into</param>
+        /// <returns>The serialised message</returns>
         public static string Serialize(object o, MessageFormat format)
         {
             var serializer = format == MessageFormat.Json
@@ -23,6 +32,13 @@ namespace CarDeliveryNetwork.Api.Data
             }
         }
 
+        /// <summary>
+        /// Deserialises the specified serialised object into instance of T
+        /// </summary>
+        /// <typeparam name="T">Type to deserialise to</typeparam>
+        /// <param name="serialisedObject">Serialised object</param>
+        /// <param name="format">Format that the serialised object</param>
+        /// <returns></returns>
         public static T Deserialise<T>(string serialisedObject, MessageFormat format)
         {
             var serializer = format == MessageFormat.Json
