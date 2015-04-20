b05b06829812f0e8ad8c979c2bff1b79177afff8
Chris Wallis <chris@inybble.com>
2015-04-20 18:13:36 +0200

    Added CdnLink.Pull example project

---
+++ cdnlink/CdnLink.Pull/CdnReceivedFtpFile.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CdnLink.Pull
+{
+    public class CdnReceivedFtpFile
+    {
+        public static string ConnectionString { get; set; }
+
+        public int Id { get; private set; }
+
+        public string Filename { get; private set; }
+
+        public string JsonMessage { get; private set; }
+
+        public int Status { get; private set; }
+
+        public static List<CdnReceivedFtpFile> GetAllQueued()
+        {
+            // Get all received files at status 60 (Queued) 
+            var sql = "SELECT f.Id, f.Filename, f.JsonMessage, r.Status FROM CdnReceives r JOIN CdnReceivedFtpFiles f ON f.Id = r.FtpFileId WHERE r.Status = 60";
+
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    var receivedFiles = new List<CdnReceivedFtpFile>();
+                    while (reader.Read())
+                    {
+                        var receivedFile = new CdnReceivedFtpFile
+                        {
+                            Id = (int)reader["Id"],
+                            Filename = reader["Filename"].ToString(),
+                            JsonMessage = reader["JsonMessage"].ToString(),
+                            Status = (int)reader["Status"]
+                        };
+                        receivedFiles.Add(receivedFile);
+                    }
+                    return receivedFiles;
+                }
+            }
+        }
+
+        public void SetAsClientProcessed()
+        {
+            // Set update to status 80 (ClientProcessed)
+            this.Status = 80;
+
+            var sql = "UPDATE CdnReceives SET Status = @Status, SuccessDate = @SuccessDate WHERE FtpFileId = @FtpFileId";
+
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add("@FtpFileId", SqlDbType.Int).Value = this.Id;
+                command.Parameters.Add("@Status", SqlDbType.Int).Value = this.Status;
+                command.Parameters.Add("@SuccessDate", SqlDbType.DateTime).Value = DateTime.Now;
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+        /// <summary>
+        /// Sets the update as errored
+        /// </summary>
+        /// <param name="errorCode">An error code for this error</param>
+        /// <param name="errorMessage">Error message</param>
+        public void SetAsError(string errorCode, string errorMessage)
+        {
+            // Set this update to status 70 (Error) andset an error message
+            this.Status = 70;
+
+            var sql = "UPDATE CdnReceives SET Status = @Status, FailedDate = @FailedDate, ErrorCode = @ErrorCode, ErrorMessage = @ErrorMessage WHERE FtpFileId = @FtpFileId";
+
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add("@FtpFileId", SqlDbType.Int).Value = this.Id;
+                command.Parameters.Add("@Status", SqlDbType.Int).Value = this.Status;
+                command.Parameters.Add("@FailedDate", SqlDbType.DateTime).Value = DateTime.Now;
+                command.Parameters.Add("@ErrorCode", SqlDbType.NVarChar).Value = errorCode;
+                command.Parameters.Add("@ErrorMessage", SqlDbType.NVarChar).Value = errorMessage;
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ cdnlink/CdnLink.Pull/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CarDeliveryNetwork.Api.Data;
+
+namespace CdnLink.Pull
+{
+    /// <summary>
+    /// Example app for pulling data from CdnLink into an instance of CarDeliveryNetwork.Api.Data.Job
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CdnReceivedFtpFile.ConnectionString = "Data Source=localhost;Initial Catalog=CdnLink;uid=CdnLinkUsr;pwd=UrsaMaj0r";
+
+            // Get the list of updates to process
+            var queuedUpdates = CdnReceivedFtpFile.GetAllQueued();
+
+            foreach (var update in queuedUpdates)
+            {
+                try
+                {
+                    // Deserialize the file contents into a job (load) object
+                    var job = Job.FromString(update.JsonMessage);
+
+
+                    //
+                    // PUT YOUR CODE HERE TO PROCESS THE JOB UPDATE
+                    //
+                    // For CarDeliveryNetwork.Api.Data.Job docs, see:
+                    // http://docs.cardeliverynetwork.com/?topic=html/T_CarDeliveryNetwork_Api_Data_Job.htm
+                    //
+
+                    throw new Exception("tatatat");
+                    // Mark the update as processed by the client
+                    update.SetAsClientProcessed();
+                }
+                catch (Exception ex)
+                {
+                    // Mark this update as errored during processing
+                    update.SetAsError("ABC123", ex.Message);
+
+                    // May want to re-throw here to exit, or continue processing other updates
+                    
+                    // throw;
+                }
+            }
+        }
+    }
+}
