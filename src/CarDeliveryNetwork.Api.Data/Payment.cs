77392a6816c269efa737a7eb8e28c311bae61e05
Chris Wallis <chris@inybble.com>
2015-09-08 17:03:46 +0200

    Added Payment

---
+++ src/CarDeliveryNetwork.Api.Data/Payment.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CarDeliveryNetwork.Api.Data
+{
+    /// <summary>
+    /// A Car Delivery Network Payment entity.
+    /// </summary>
+    public class Payment : ApiEntityBase<Payment>
+    {
+        /// <summary>
+        /// InvoiceNumber
+        /// </summary>
+        public virtual string InvoiceNumber { get; set; }
+
+        /// <summary>
+        /// InvoiceNumber2
+        /// </summary>
+        public virtual string InvoiceNumber2 { get; set; }
+    }
+}
