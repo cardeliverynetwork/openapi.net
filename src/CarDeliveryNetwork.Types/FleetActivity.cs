323995f88b8e89a575d54d127108e24c52d2eef0
Chris Wallis <chris@inybble.com>
2015-12-09 12:37:13 +0100

    Added enums FleetActivity and FleetLicence

---
+++ src/CarDeliveryNetwork.Types/FleetActivity.cs
@@ -0,0 +1,54 @@
+﻿
+namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum representing activity type of a Fleet
+    /// </summary>
+    public enum FleetActivity
+    {
+        /// <summary>
+        /// Oem
+        /// </summary>
+        Oem,
+
+        /// <summary>
+        /// NewCarDealer
+        /// </summary>
+        NewCarDealer,
+
+        /// <summary>
+        /// SecondaryDealer
+        /// </summary>
+        SecondaryDealer,
+
+        /// <summary>
+        /// DailyRental
+        /// </summary>
+        DailyRental,
+
+        /// <summary>
+        /// Broker
+        /// </summary>
+        Broker,
+
+        /// <summary>
+        /// OnetoTwoCarTransporters
+        /// </summary>
+        OnetoTwoCarTransporters,
+
+        /// <summary>
+        /// MultiCarCarrier
+        /// </summary>
+        MultiCarCarrier,
+
+        /// <summary>
+        /// DrivenService
+        /// </summary>
+        DrivenService,
+
+        /// <summary>
+        /// TransportedAndDriven
+        /// </summary>
+        TransportedAndDriven
+    }
+}
---
+++ src/CarDeliveryNetwork.Types/FleetLicence.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// FleetLicence
+    /// </summary>
+    public enum FleetLicence
+    {
+        /// <summary>
+        /// Trial
+        /// </summary>
+        Trial,
+
+        /// <summary>
+        /// Credit
+        /// </summary>
+        Credit = 10,
+
+        /// <summary>
+        /// Contract
+        /// </summary>
+        Contract = 20,
+    }
+}
