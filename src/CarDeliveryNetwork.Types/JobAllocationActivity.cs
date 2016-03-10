f02e0f0d76530007fd7ddb64158f6110287bc061
Sagar Thapa <sagarthapa_9@yahoo.com>
2016-03-10 13:07:35 +0000

    Added JOb Allocation Activity Type

---
+++ src/CarDeliveryNetwork.Types/JobAllocationActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum of types of job allocation.
+    /// </summary>
+    public enum JobAllocationActivity
+    {
+        /// <summary>
+        /// Claimed by owner
+        /// </summary>
+        ClaimOwn,
+
+        /// <summary>
+        /// Direct Allocated.
+        /// </summary>
+        DirectAllocate,
+
+        /// <summary>
+        /// Claimed.
+        /// </summary>
+        Claim,
+
+        /// <summary>
+        /// Bid Placed.
+        /// </summary>
+        Bid
+
+    }
+}
+
+
