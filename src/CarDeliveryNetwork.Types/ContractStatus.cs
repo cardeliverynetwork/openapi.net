f3512768171b082d8dac753efb2c5471a4967ac5
Alan Stallwood <alan.stallwood@gmail.com>
2016-03-23 17:46:02 +0000

    New types for nomad work

---
+++ src/CarDeliveryNetwork.Types/ContractStatus.cs
@@ -0,0 +1,31 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum of Car Delivery Network contract status
+    /// </summary>
+    public enum ContractStatus
+    {
+        // Start State
+        /// <summary>
+        /// Open
+        /// </summary>
+        Open = 0,
+
+        // Middling states
+        /// <summary>
+        /// Declined
+        /// </summary>
+        Declined = 20,
+
+        /// <summary>
+        /// Claimed
+        /// </summary>
+        Claimed = 30,
+
+        // End States
+        /// <summary>
+        /// Approved
+        /// </summary>
+        Approved = 40
+    }
+}
