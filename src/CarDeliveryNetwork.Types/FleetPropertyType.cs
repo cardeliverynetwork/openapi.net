8820802d9f40f51b1f46eb425a7a5a494d680817
Alan Stallwood <alan.stallwood@gmail.com>
2016-03-23 09:01:42 +0000

    Fleet properties enum

---
+++ src/CarDeliveryNetwork.Types/FleetPropertyType.cs
@@ -0,0 +1,188 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Enum of Car Delivery Network fleet properties
+    /// </summary>
+    public enum FleetPropertyType
+    {
+        /// <summary>
+        /// ReceiveNewsletter
+        /// </summary>
+        ReceiveNewsletter,
+
+        /// <summary>
+        /// MenuProfile
+        /// </summary>
+        MenuProfile,
+
+        // These used to be the SACFleetProperties before hierachy was flattened
+
+        /// <summary>
+        /// CanDoEuropeanMovements
+        /// </summary>
+        CanDoEuropeanMovements = 1000,
+
+        /// <summary>
+        /// HasSecureStorage
+        /// </summary>
+        HasSecureStorage,
+
+        /// <summary>
+        /// HasNonSecureStorage
+        /// </summary>
+        HasNonSecureStorage,
+
+        /// <summary>
+        /// HasPdiServices
+        /// </summary>
+        HasPdiServices,
+
+        /// <summary>
+        /// HasTechnicalServices
+        /// </summary>
+        HasTechnicalServices,
+
+        /// <summary>
+        /// HasAuctionServices
+        /// </summary>
+        HasAuctionServices,
+
+        /// <summary>
+        /// CanDoPrestige
+        /// </summary>
+        CanDoPrestige,
+
+        /// <summary>
+        /// CanDoClassic
+        /// </summary>
+        CanDoClassic,
+
+        /// <summary>
+        /// CanDoNonRunners
+        /// </summary>
+        CanDoNonRunners,
+
+        /// <summary>
+        /// MultiCarOpen
+        /// </summary>
+        MultiCarOpen,
+
+        /// <summary>
+        /// TwoCarOpen
+        /// </summary>
+        TwoCarOpen,
+
+        /// <summary>
+        /// OneCarOpen
+        /// </summary>
+        OneCarOpen,
+
+        /// <summary>
+        /// MultiCarEnclosed
+        /// </summary>
+        MultiCarEnclosed,
+
+        /// <summary>
+        /// TwoCarEnclosed
+        /// </summary>
+        TwoCarEnclosed,
+
+        /// <summary>
+        /// OneCarEnclosed
+        /// </summary>
+        OneCarEnclosed,
+
+        /// <summary>
+        /// OpenTrailer
+        /// </summary>
+        OpenTrailer,
+
+        /// <summary>
+        /// EnclosedTrailer
+        /// </summary>
+        EnclosedTrailer,
+
+        /// <summary>
+        /// DotNumber
+        /// </summary>
+        DotNumber,
+
+        /// <summary>
+        /// McNumber
+        /// </summary>
+        McNumber,
+
+        /// <summary>
+        /// EinTaxId
+        /// </summary>
+        EinTaxId,
+
+        /// <summary>
+        /// SaferStatRating
+        /// </summary>
+        SaferStatRating,
+
+        /// <summary>
+        /// StateDotNumber
+        /// </summary>
+        StateDotNumber,
+
+        /// <summary>
+        /// BusinessStartedYear
+        /// </summary>
+        BusinessStartedYear,
+
+        /// <summary>
+        /// AgentsTelephone
+        /// </summary>
+        AgentsTelephone,
+
+        /// <summary>
+        /// AgentsCompany
+        /// </summary>
+        AgentsCompany,
+
+        /// <summary>
+        /// PowerUnitDrivers
+        /// </summary>
+        PowerUnitDrivers,
+
+        /// <summary>
+        /// RegisteredState
+        /// </summary>
+        RegisteredState,
+
+        /// <summary>
+        /// BondCompany
+        /// </summary>
+        BondCompany,
+
+        /// <summary>
+        /// BondCompanyTelephone
+        /// </summary>
+        BondCompanyTelephone,
+
+        /// <summary>
+        /// BondAmount
+        /// </summary>
+        BondAmount,
+
+        // Fleet settings
+
+
+        /// <summary>
+        /// SendsInstantQuoteOnRequest
+        /// </summary>
+        SendsInstantQuoteOnRequest = 1100,
+
+        /// <summary>
+        /// CanIssueCustomerLogin
+        /// </summary>
+        CanIssueCustomerLogin,
+
+        /// <summary>
+        /// ClaimOnDeviceComplete
+        /// </summary>
+        ClaimOnDeviceComplete
+    }
+}
