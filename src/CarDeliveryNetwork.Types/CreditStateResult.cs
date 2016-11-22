43a6a8ddccd7ecc9e764e341b4d22523923e68b7
Alan Stallwood <alan.stallwood@gmail.com>
2016-11-22 17:58:32 +0000

    CreditStateResult type added

---
+++ src/CarDeliveryNetwork.Types/CreditStateResult.cs
@@ -0,0 +1,21 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    /// <summary>
+    /// Possible fleet credit levels
+    /// </summary>
+    public enum CreditStateResult
+    {
+        /// <summary>
+        /// Good level
+        /// </summary>
+        Good,
+        /// <summary>
+        /// Low level
+        /// </summary>
+        Warn,
+        /// <summary>
+        /// No credit
+        /// </summary>
+        Out,
+    }
+}
