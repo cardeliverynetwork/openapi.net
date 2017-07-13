4c4196cfee1cc8d7e80ea04bb6ad27a2caf627b5
Sagar Thapa <sagarthapa_9@yahoo.com>
2017-07-13 22:02:38 +0100

    updated submodule

--- src/CarDeliveryNetwork.Api.Data/Alert.cs
+++ src/CarDeliveryNetwork.Api.Data/Alert.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace CarDeliveryNetwork.Api.Data
@@ -16,34 +17,82 @@ namespace CarDeliveryNetwork.Api.Data
         /// <summary>
         /// A description of the alert
         /// </summary>
-        public virtual string Description { get; set; }
-    }
-
-    /// <summary>
-    /// A collection of Car Delivery Network Alert entities.
-    /// </summary>
-    [CollectionDataContract]
-    public class Alerts : ApiEntityCollectionBase<Alert, Alerts>
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.Alerts"/> class
-        /// that is empty and has the default initial capacity.
-        /// </summary>
-        public Alerts() { }
+        public virtual string MessageBody { get; set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.Alerts"/> class
-        /// that is empty and has the specified initial capacity.
+        /// An associated JobId of the alert
         /// </summary>
-        /// <param name="capacity">The number of elements that the new list can initially store</param>
-        public Alerts(int capacity) : base(capacity) { }
+        public int AssociatedJobId { get; set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.Alerts"/> class
-        /// that contains elements copied from the specified collection and has sufficient
-        /// capacity to accommodate the number of elements copied.
+        /// An associated Fleet Id of the alert
         /// </summary>
-        /// <param name="alerts">The collection of Alerts whose elements are copied to the new collection.</param>
-        public Alerts(IEnumerable<Alert> alerts) : base(alerts) { }
+        public int FleetId { get; set; }
+
+        /// <summary>
+        /// A vindispatch alert Type 
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// A date of alert creation
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// A fleet from whom alert is created
+        /// </summary>
+        public string FleetFrom { get; set; }
+
+        /// <summary>
+        /// An user from whom alert is  created
+        /// </summary>
+        public string UserFrom { get; set; }
+
+
+        /// <summary>
+        /// An fleet to whom alert is  sent
+        /// </summary>
+        public string FleetTo { get; set; }
+
+        /// <summary>
+        /// An user to whom alert is  sent
+        /// </summary>
+        public string UserTo { get; set; }
+
+
+        /// <summary>
+        /// A collection of Car Delivery Network Alert entities.
+        /// </summary>
+        [CollectionDataContract]
+        public class Alerts : ApiEntityCollectionBase<Alert, Alerts>
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.Alerts"/> class
+            /// that is empty and has the default initial capacity.
+            /// </summary>
+            public Alerts()
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.Alerts"/> class
+            /// that is empty and has the specified initial capacity.
+            /// </summary>
+            /// <param name="capacity">The number of elements that the new list can initially store</param>
+            public Alerts(int capacity) : base(capacity)
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CarDeliveryNetwork.Api.Data.Alerts"/> class
+            /// that contains elements copied from the specified collection and has sufficient
+            /// capacity to accommodate the number of elements copied.
+            /// </summary>
+            /// <param name="alerts">The collection of Alerts whose elements are copied to the new collection.</param>
+            public Alerts(IEnumerable<Alert> alerts) : base(alerts)
+            {
+            }
+        }
     }
 }
