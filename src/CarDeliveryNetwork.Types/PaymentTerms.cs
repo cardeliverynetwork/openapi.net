90a965b41a66aa95795c77290d0bdcabae02270b
Pengxin Fan <fanpengxin@hotmail.com>
2018-10-03 09:29:28 +0100

    add PaymentTerms enum

---
+++ src/CarDeliveryNetwork.Types/PaymentTerms.cs
@@ -0,0 +1,30 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    public enum PaymentTerms
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        COD,
+
+        /// <summary>
+        /// 15 days
+        /// </summary>
+        NET15,
+
+        /// <summary>
+        /// 30 days
+        /// </summary>
+        NET30,
+
+        /// <summary>
+        /// 60 days
+        /// </summary>
+        NET60,
+
+        /// <summary>
+        /// 90 days
+        /// </summary>
+        NET90
+    }
+}
