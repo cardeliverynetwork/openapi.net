10f9e5ba3ea9a92d49867908878c891c34c6e87b
Chris Wallis <chris@inybble.com>
2019-04-04 13:02:51 +0100

    Added missing XML comments

--- src/CarDeliveryNetwork.Types/DocViewMode.cs
+++ src/CarDeliveryNetwork.Types/DocViewMode.cs
@@ -1,10 +1,24 @@
 ﻿
 namespace CarDeliveryNetwork.Types
 {
+    /// <summary>
+    /// DocViewMode
+    /// </summary>
     public enum DocViewMode
     {
+        /// <summary>
+        /// View
+        /// </summary>
         View,
+
+        /// <summary>
+        /// Print
+        /// </summary>
         Print,
+
+        /// <summary>
+        /// Download
+        /// </summary>
         Download
     }
 }
