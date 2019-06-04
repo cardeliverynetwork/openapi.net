9488b6a23a399a1af5d3c8c1aa5e9fd859c4bed4
Alan Stallwood <alan.stallwood@gmail.com>
2019-06-04 14:05:17 +0100

    [axof: 86] Change to CdxShipment status

---
+++ src/CarDeliveryNetwork.Types/CdxShipmentStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace CarDeliveryNetwork.Types
+{
+    public enum CdxShipmentStatus
+    {
+        ExchangeCreated = 0,
+        ReceivedByDriver = 10,
+        OnWayToPickup = 20,
+        PickupOnHold = 30,
+        AtPickUp = 40,
+        PickedUp = 50,
+        NotPickedUp = 60,
+        OnWayToDeliver = 70,
+        DeliveryOnHold = 80,
+        Delivered = 90,
+        NotDelivered = 100,
+        Tracking = 110
+    }
+}
