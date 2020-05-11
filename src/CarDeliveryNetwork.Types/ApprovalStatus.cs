2c56b90bc12c508d8b6c421bc5bfca56610b971a
Chris Wallis <chris@inybble.com>
2020-05-11 16:44:22 +0100

    [axof:855] Added ApprovalStatus enum

---
+++ src/CarDeliveryNetwork.Types/ApprovalStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarDeliveryNetwork.Types
+{
+    public enum ApprovalStatus
+    {
+        Refused = 0,
+        Pending = 10,
+        Approved = 20
+    }
+}
