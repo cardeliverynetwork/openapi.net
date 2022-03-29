3407dc21056ba880af7154f4e1bf5c725b8e9a42
Alan Stallwood <alanstallwood@gmail.com>
2022-03-29 09:30:07 +0100

    New functionality flags

--- src/CarDeliveryNetwork.Types/JobFunctionalityFlags.cs
+++ src/CarDeliveryNetwork.Types/JobFunctionalityFlags.cs
@@ -8,17 +8,9 @@ namespace CarDeliveryNetwork.Types
     [Flags]    
     public enum JobFunctionalityFlags
     {
-        MustScanOnLoad = 1,
-        MustScanOnUnload = 2,
-        ScanNewCar = 4,
-        AddNewCar = 8,
-        AbortLoad = 16,
-        AbortVehiclePickup = 32,
-        LoadAllVehicles = 64,
-        UnloadAllVehicles = 128,
-        MustDoDutyOfCare = 256,
-        MustDoPaperwork = 512,
-        MustTakeOtherPhotos = 1024,
-        MustGetDamageAgentSignoff = 2048
+        DisableAbortLoad = 1,
+        DisableExpenses = 2,
+        DisableAddVehicle = 4,
+        DisableAbortVehicle = 8
     }
 }
